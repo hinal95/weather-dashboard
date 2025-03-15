96093baecce6553f06621155189935295326b5e7
hinal95 <hinalnagar95@gmail.com>
2025-03-15 15:46:33 +0530

    Update app.js
    this file is not needed now

--- public/app.js
+++ public/app.js
@@ -1,17 +1,1 @@
-//http://api.weatherapi.com/v1/current.json?key=5d58e46b0d7a472aae4103332251403&q=delhi&aqi=no
 
-let target = "mumbai"
-const fetchData = async function(targetlocation){
-    let url = `http://api.weatherapi.com/v1/current.json?key=5d58e46b0d7a472aae4103332251403&q=${targetlocation}&aqi=no`
-    const response = await fetch(url)
-    const data = response.json()
-    console.log(data)
-
-    //  let locationName = data.location.name
-    //  console.log(locationName)
-     
-     let time = data.location.localtime
-     console.log(time)
-
-}
-fetchData(target)
